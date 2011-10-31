751f9f8f71300b9941af9075d6bc8200f6850d65
Takeru Ohta <phjgt308@gmail.com>
2011-10-31 19:53:26 +0900

    経過保存

--- merge-sort.lisp
+++ merge-sort.lisp
@@ -14,11 +14,9 @@
   (labels ((less-equal-than (list1 list2)
              (not (funcall test (funcall key (car list2)) (funcall key (car list1)))))
            (recur (tail l1 l2)
-             (cond ((null l1) (cdr! tail l2) 
-                    (prog1 (cdr head)
-                      (cdr! head next)))
-                   ((less-equal-than l1 l2)  (recur (cdr! tail l1) (cdr l1) l2))
-                   (t                        (recur (cdr! tail l2) (cdr l2) l1)))))
+             (cond ((null l1)               (cdr! tail l2) (shiftf (cdr head) next))
+                   ((less-equal-than l1 l2) (recur (cdr! tail l1) (cdr l1) l2))
+                   (t                       (recur (cdr! tail l2) (cdr l2) l1)))))
     (declare (inline less-equal-than))
     (recur head list1 list2)))
 
@@ -26,14 +24,12 @@
   (declare (list list)
            (function test key)
            (optimize (speed 3) (safety 2) (debug 2)))
-  (labels ((recur (list size &aux (half (ash size -1)))
+  (labels ((recur (list size &aux (half1 (ash size -1)) (half2 (- size half1)))
              (declare (optimize (speed 3) (safety 0) (debug 0))
-                      (fixnum size half))
+                      (fixnum size half1 half2))
              (if (= 1 size)
                  (shiftf (cdr head) (cdr list) nil)
-               (merge-lists head 
-                            (recur list half) (recur (cdr head) (- size half))
-                            test key))))
+               (merge-lists head (recur (cdr head) half1) (recur (cdr head) half2) test key))))
     (when list
       (values (recur list (length list))))))
 

ae0cafa505e62cedd7bb1a29092dcc9b79ff091c
Takeru Ohta <phjgt308@gmail.com>
2011-11-02 04:43:15 +0900

    multiple-value-bindを使用するコードに復元

--- merge-sort.lisp
+++ merge-sort.lisp
@@ -6,13 +6,13 @@
 
 (declaim (inline merge-lists sort-impl))
 
-(defun merge-lists (head list1 list2 test key &aux (next (cdr head)) (tail head))
+(defun merge-lists (head list1 list2 test key &aux (tail head))
   (macrolet ((less-equal-than (l1 l2)
                `(not (funcall test (funcall key (car ,l2)) (funcall key (car ,l1)))))
              (merge-one (l1 l2)
                `(unless (setf ,l1 (cdr (setf tail (setf (cdr tail) ,l1))))
                   (setf (cdr tail) ,l2)
-                  (return (shiftf (cdr head) next)))))
+                  (return (cdr head)))))
     (loop
      (if (less-equal-than list1 list2)
          (merge-one list1 list2)
@@ -22,16 +22,17 @@
 (defun sort-impl (list test key &aux (head (cons :head list)))
   (declare (optimize (speed 3) (safety 2) (debug 2))
            (dynamic-extent head))
-  (labels ((recur (size)
+  (labels ((recur (list size)
              (declare (optimize (speed 3) (safety 0) (debug 0))
                       (fixnum size)) 
              (if (= 1 size)
-                 (let ((next (cdr head)))
-                   (shiftf (cdr head) (cdr next) nil))
+                 (values list (shiftf (cdr list) nil))
                (let ((half (ash size -1)))
-                 (merge-lists head (recur half) (recur (- size half)) test key)))))
+                 (multiple-value-bind (list1 rest) (recur list half)
+                   (multiple-value-bind (list2 rest) (recur rest (- size half))
+                     (values (merge-lists head list1 list2 test key) rest)))))))
     (when list
-      (recur (length list)))))
+      (values (recur list (length list))))))
 
 (defun sort (list test &key (key #'identity) inline)
   (declare (ignore inline))
